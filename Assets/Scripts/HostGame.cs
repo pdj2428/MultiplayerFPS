1c310c654c119dcec9fa406655db43315b7ac712
pdj2428 <49777470+pdj2428@users.noreply.github.com>
2020-10-08 13:46:36 +0900

    repository change
    .

---
+++ Assets/Scripts/HostGame.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Mirror;
+using System.Net.Sockets;
+
+public class HostGame : MonoBehaviour
+{
+    [SerializeField]
+    private int roomSize = 6;
+
+    private string createRoomName;
+    private string joinRoomName;
+
+    private NetworkManager networkManager;
+    
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+    }
+
+    public void SetCreateRoomName (string _name)
+    {
+        createRoomName = _name;
+    }
+
+    public void CreateRoom ()
+    {
+        if (createRoomName != "" && createRoomName != null)
+        {
+            Debug.Log("Creating Room: " + createRoomName + " with room for " + roomSize + " Players");
+
+            networkManager.maxConnections = roomSize;
+            networkManager.networkAddress = createRoomName;
+            networkManager.StartHost();
+        }
+    }
+
+    public void SetJoinRoomName(string _name)
+    {
+        joinRoomName = _name;
+    }
+
+    public void JoinRoom()
+    {
+        if (joinRoomName != "" && joinRoomName != null)
+        {
+            Debug.Log("Joinning Room: " + joinRoomName + " with room for " + roomSize + " Players");
+
+            networkManager.networkAddress = createRoomName;
+            networkManager.StartClient();
+        }
+    }
+}
